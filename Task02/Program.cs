8000d373379079f85da957834c5f8cf190df4060
miracaptured <k.sivka@yandex.ru>
2020-11-14 22:46:34 +0300

    I hope it works..

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -22,24 +22,57 @@ namespace Task02
         private const string inputPath = "input.txt";
         private const string outputPath = "output.txt";
         
+       
         static int[] ReadFile(string path)
         {
-            // TODO: implement this method
+            string items = File.ReadAllText(path);
+            string[] values = items.Split(' ');
+            int[] result = new int[values.Length];
+            int i = 0;
+            foreach (string item in values)
+            {
+                int.TryParse(item, out result[i++]);
+            }
+            return result;
         }
         
         static bool CheckArray(int[] array)
         {
-            // TODO: implement this method
+            foreach (int item in array)
+            {
+                if (item < 2 || item > 10000)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
         
         static int[] ConvertArray(int[] array)
         {
-            // TODO: implement this method
+            int[] powersArray = new int[array.Length];
+            int i = 0;
+            foreach (int item in array)
+            {
+                int pow = 1;
+                while (item > pow * 2)
+                {
+                    pow *= 2;
+                }
+                powersArray[i++] = pow;
+            }
+            return powersArray;
         }
+
 
         static void WriteFile(string path, int[] array)
         {
-            // TODO: implement this method
+            string result = String.Empty;
+            foreach (int item in array)
+            {
+                result += $"{item} ";
+            }
+            File.WriteAllText(path, result);
         }
 
         // you do not need to fill your file manually, you can work with console input
@@ -50,23 +83,25 @@ namespace Task02
             
             int[] A;
             int[] B;
-            
             try
             {
                 A = ReadFile(inputPath);
-                
                 if (!CheckArray(A))
-                    // TODO: implement this case
-                
+                {
+                    Console.WriteLine("Incorrect Input");
+                    return;
+                }
                 B = ConvertArray(A);
                 WriteFile(outputPath, B);
             }
-            // TODO: catch with meaningful message
-            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             // do not touch
             ConsoleOutput();
         }
-        
+
         #region Testing methods for Github Classroom, do not touch!
 
         static void FillFile()
